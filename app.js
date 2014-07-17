e73b27ab59f3894f2b8dd1e2a244ec9bf9dc4260
Peter Savichev (proton) <psavichev@gmail.com>
2014-07-17 23:38:41 +0400

    added routing

--- app.js
+++ app.js
@@ -7,30 +7,33 @@ obj_to_arr = function (obj) {
 };
 
 // Object.values = function (obj) {
-//     var vals = [];
-//     for( var key in obj ) {
-//         if ( obj.hasOwnProperty(key) ) {
-//             vals.push(obj[key]);
-//         }
-//     }
-//     return vals;
+//		 var vals = [];
+//		 for( var key in obj ) {
+//				 if ( obj.hasOwnProperty(key) ) {
+//						 vals.push(obj[key]);
+//				 }
+//		 }
+//		 return vals;
 // }
-
-// fotorama_config = {
-
-// };
-
-// function photo_to_item(photo)
-// {
-// 	return {img: photo.img.XL.href, thumb: photo.img.L.href, full: photo.img.orig.href};
-// }
-
-// var load_count = 0;
 
 (function(){
 	var app = angular.module('app', ['ngRoute']);
 
-	app.controller('PhotosController', ['$scope', '$http', '$window', function($scope, $http, $window){
+	// app.config(function($routeProvider) {
+	// 	$routeProvider
+	// 		.when('/', {resolve: {redirect: 'PhotosController'}})
+	// 		.when('/albums/:album_id', {
+	// 			template: '',
+	// 			controller: 'PhotosController'
+	// 		})
+	// 		.when('/tags/:tag_id', {
+	// 			template: '',
+	// 			controller: 'PhotosController'
+	// 		})
+	// 		.otherwise({ redirectTo:'/' });
+	// })
+
+	app.controller('PhotosController', ['$scope', '$location', '$routeParams', '$http', '$window', function($scope, $location, $routeParams, $http, $window){
 		$scope.albums = [];
 		$scope.tags = [];
 		$scope.photos = [];
@@ -42,136 +45,109 @@ obj_to_arr = function (obj) {
 		$scope.code_url = null;
 		$scope.code_thumb = null;
 
+		var base_url = config.url+'/users/'+config.user;
 		//TODO: грузит не все фотки, а только 100 первых
-		var url = config.url+'/users/'+config.user+'/albums/?format=json&callback=JSON_CALLBACK';
+		var url = base_url+'/albums/?format=json&callback=JSON_CALLBACK';
 		$http({ method: "JSONP", url: url}).success(function(data){
 			$scope.albums = data.entries;
 		});
 		//TODO: грузит не все, а только 100 первых
-		var url = config.url+'/users/'+config.user+'/tags/?format=json&callback=JSON_CALLBACK';
+		var url = base_url+'/tags/?format=json&callback=JSON_CALLBACK';
 		$http({ method: "JSONP", url: url}).success(function(data){
 			$scope.tags = data.entries;
 		});
+		url = base_url;
 		//TODO: грузит не все, а только 100 первых
-		var url = config.url+'/users/'+config.user+'/photos/?format=json&callback=JSON_CALLBACK';
+		if($scope.album_id)
+		{
+			url += '/album/'+$scope.album_id;
+		}
+		else if($scope.tag_id)
+		{
+			url += '/tag/'+$scope.tag_id;
+		}
+		url += '/photos/?format=json&callback=JSON_CALLBACK';
 		$http({ method: "JSONP", url: url}).success(function(data){
+			// console.log(url);
 			$scope.photos = data.entries;
+			// console.log($scope.photos.length);
 		});
-		// $scope.$on('$imageLoaded', function () {
-		// 	if (load_count++ === $scope.photos.length - 1) {
-		// 		console.log('finish');
-		// 		$('.fotorama').fotorama();
-		// 	}
-		// });
 
-			this.setPhoto = function(){
-				$scope.photo = ($scope.photos.length==0 || $scope.index==-1) ? null : $scope.photos[$scope.index];
-				if($scope.photo)
-				{
-					var f = function(img){ return img.width>=$window.innerWidth; };
-					var p = obj_to_arr($scope.photo.img).filter(f)[0] || $scope.photo.img.orig;
-					$scope.current_image = p.href;
-				}
-				else
-				{
-					$scope.current_image = null;
-				}
-			};
+		this.setPhoto = function(){
+			$scope.photo = ($scope.photos.length==0 || $scope.index==-1) ? null : $scope.photos[$scope.index];
+			if($scope.photo)
+			{
+				var f = function(img){ return img.width>=$window.innerWidth; };
+				var p = obj_to_arr($scope.photo.img).filter(f)[0] || $scope.photo.img.orig;
+				$scope.current_image = p.href;
+			}
+			else
+			{
+				$scope.current_image = null;
+			}
+		};
 
-			// $scope.photo = function(){
-			// 	if($scope.photos.length==0) return null;
-			// 	return $scope.photos[$scope.photo_index];
-			// };
+		// $scope.photo = function(){
+		// 	if($scope.photos.length==0) return null;
+		// 	return $scope.photos[$scope.photo_index];
+		// };
 
-			this.setIndex = function(index){
-				$scope.index = index;
-				this.setPhoto();
-			};
+		this.setIndex = function(index){
+			$scope.index = index;
+			this.setPhoto();
+		};
 
-			$scope.$watch(
-				function(){ console.log(123); return $window.innerWidth; },
-				function(value){ console.log(value); }
-			);
+		$scope.to_id = function(t){
+			return decodeURI(t.id.split(':')[5]);
+		}
+
+		// $scope.$watch(
+		// 	function(){ return $window.innerWidth; },
+		// 	function(value){ console.log(value); }
+		// );
+
+		$scope.$on('$locationChangeSuccess', function (event) {
+			var location_array = $location.$$path.split('/');
+			$scope.album_id = location_array[1]=='albums' ? location_array[2] : null;
+			$scope.tag_id = location_array[1]=='tags' ? location_array[2] : null;
+		});
 	}]);
 
 	// app.directive('loadDispatcher', function() {
- //  	return {
- //      restrict: 'A',
- //      link: function($scope, element, attrs) {
- //      		element.bind('load', function() {
- //          $scope.$emit('$imageLoaded');
- //        });
- //      }
- //    };
+ //		return {
+ //			restrict: 'A',
+ //			link: function($scope, element, attrs) {
+ //					element.bind('load', function() {
+ //					$scope.$emit('$imageLoaded');
+ //				});
+ //			}
+ //		};
 	// })
 })();
 
 
 
-		// app.directive('tagsManager', function () {
-		//  return {
-		//      restrict: 'A',
-		//      link: function (scope, element, attrs) {
-		//        $('.fotorama').fotorama();
-		//        console.log(c);
-		//      }
-		//    }
-		//  });
+// app.directive('tagsManager', function () {
+//	return {
+//			restrict: 'A',
+//			link: function (scope, element, attrs) {
+//				$('.fotorama').fotorama();
+//				console.log(c);
+//			}
+//		}
+//	});
 
-		// $scope.options = {
-		// 	width: '100%',
-		// 	height: 400,
-		// 	loop: true,
-		// 	keyboard: true,
-		// 	nav: 'thumbs',
-		// 	load: function (e, extra) {
-		//     console.log(extra); //{user: undefined/true} дополнительные данные
-		//   },
-		// };
+// $scope.options = {
+// 	width: '100%',
+// 	height: 400,
+// 	loop: true,
+// 	keyboard: true,
+// 	nav: 'thumbs',
+// 	load: function (e, extra) {
+//		 console.log(extra); //{user: undefined/true} дополнительные данные
+//	 },
+// };
 
-		// this.thumb = function(photo){
-		// 	return photo.img.M.width>=300 ? photo.img.M : photo.img.L;
-		// }
-
-// .config(function($routeProvider) {
-//   $routeProvider
-//     .when('/', {
-//       controller: 'PhotosController',
-//       templateUrl: 'photos.htm'
-//     })
-//     // .when('/edit/:projectId', {
-//     //   controller:'EditCtrl',
-//     //   templateUrl:'detail.html'
-//     // })
-//     // .when('/new', {
-//     //   controller:'CreateCtrl',
-//     //   templateUrl:'detail.html'
-//     // })
-//     .otherwise({ redirectTo:'/' });
-// })
-// .controller('PhotosController', function($scope, Photos) {
-// 	console.log(123);
-//   $scope.photos = Photos;
-// });
-
-
-// var app = angular.module('yaph', []);
-	
-// var album = {
-// 	id: 158281,
-// 	title: 'Шестая всероссийская Встреча Лаборатории Фантастики'
+// this.thumb = function(photo){
+// 	return photo.img.M.width>=300 ? photo.img.M : photo.img.L;
 // }
-
-// var photo = {
-// 	url: 'http://img-fotki.yandex.ru/get/9251/25165086.1e/0_ae89d_3e69c70c_XXL'
-// }
-
-// app.controller('AlbumsController', function(){
-//   this.album = album;
-// });
-
-// app.controller('PhotosController', function(){
-//   this.photo = photo;
-// });
-
-// api_url+'/users/'+user+'/?format=json&callback=callback'
